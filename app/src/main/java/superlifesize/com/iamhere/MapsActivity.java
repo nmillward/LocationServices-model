8d4a23a7859bc97c43fd7f29f7eb5cab1a0f038c
Nick Millward <nicholasmillward@gmail.com>
2015-01-27 21:37:59 -0500

    Cleanup on MapsActivity class.

--- app/src/main/java/superlifesize/com/iamhere/MapsActivity.java
+++ app/src/main/java/superlifesize/com/iamhere/MapsActivity.java
@@ -83,7 +83,6 @@ public class MapsActivity extends FragmentActivity implements LocationProvider.L
 
     }
 
-
     public void handleNewLocation(Location location) {
         Log.d(TAG, location.toString());
 
